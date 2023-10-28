bfcd6d899379a08e4eb4a6656c199581935e7b9d
rtef119 <rtef119@gmail.com>
2023-10-28 20:16:58 +0200

    starvation

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ class satiger:
 
 class tiger(satiger):
     age = 60
+    horrible = 100
 
 class kat(tiger):
     heigth = 30
+    Bread = 60
     def __init__(self):
         print("Зріст",self.heigth)
         print("Голод" ,self.starvation)
         print("Рік" ,self.age)
+        print("Лячність", self.horrible)
+        print("Хліб", self.Bread)
 
 ra = kat()
